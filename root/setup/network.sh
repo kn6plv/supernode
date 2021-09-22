98369770c13c572ea113cbe5cd5b4d1ee2d90062
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-09-21 21:21:52 -0700

    Static ip handling

--- root/setup/network.sh
+++ root/setup/network.sh
@@ -5,6 +5,8 @@ cat > /etc/resolv.conf << __EOF__
 nameserver 1.1.1.1
 __EOF__
 
-# Create a dummy interface we can put our PRIMARY_IP on
-ip link add link eth0 name primary type vlan id 3072
-ifconfig primary ${PRIMARY_IP} netmask 255.0.0.0 broadcast 10.255.255.255
+if [ "${__DEFAULT_INTERFACE_IP}" != "${PRIMARY_IP}" -a "${__SECONDARY_INTERFACE_IP}" != "${PRIMARY_IP}" ]; then
+  # Create a dummy interface we can put our PRIMARY_IP on
+  ip link add link eth0 name primary type vlan id 3072
+  ip addr add ${PRIMARY_IP}/8 broadcast + dev primary
+fi
