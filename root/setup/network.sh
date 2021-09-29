4cc0179f7b3c9d9ce53fff4c7200fa8184e07135
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-09-29 10:47:19 -0700

    Add ip masquarading

--- root/setup/network.sh
+++ root/setup/network.sh
@@ -1,5 +1,7 @@
 # Network
 
+IPTABLES=iptables-legacy
+
 # Override the nameserver (just in case)
 cat > /etc/resolv.conf << __EOF__
 nameserver 1.1.1.1
@@ -10,4 +12,12 @@ if [ "${__DEFAULT_INTERFACE_IP}" != "${PRIMARY_IP}" -a "${__SECONDARY_INTERFACE_
   ip link add link eth0 name primary type vlan id 3072
   ip addr add ${PRIMARY_IP}/8 broadcast + dev primary
   ip link set primary up
+fi
+
+# Enable masquarading on isolated interfaces, so we hide the connected network details from the mesh
+if [ "${ENABLE_MASQUARADE}" = "true" ]; then
+  for net in ${MESH_NETS}
+  do
+    ${IPTABLES} -t nat -I POSTROUTING -o ${net} -j MASQUERADE
+  done
 fi
